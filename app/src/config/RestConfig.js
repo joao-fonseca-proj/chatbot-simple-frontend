afb9d5fd0abc2196846cfa6f6556f3896f3c58c4
João Fonseca <1171166@isep.ipp.pt>
2022-05-14 17:10:17 +0100

    Refactored base project

--- app/src/config/RestConfig.js
+++
@@ -1,1 +0,0 @@
-export const BASE_URI = 'http://20.199.125.176:8080';
--- app/src/service/ChatbotService.js
+++
@@ -1,7 +0,0 @@
-import { BASE_URI } from "../config/RestConfig";
-import axios from 'axios'
-
-
-export function postMessage(message) {
-    return axios.post(BASE_URI + "/predict", { text: message});
-}
